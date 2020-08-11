513108f5cae3761cf44a731204e991ad4c0852ec
barbara_workman <susan.workman90@gmail.com>
2020-08-11 10:36:00 -0400

    Updates

---
+++ precache-manifest.e1ce0bba6e4c6a0fd8735e32765a7348.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77c03d09d5bbd14e07099f9769433ba",
+    "url": "/react-sports-game-assessment/index.html"
+  },
+  {
+    "revision": "a5ffce9fe61eddb0eb78",
+    "url": "/react-sports-game-assessment/static/css/main.b38e3ed9.chunk.css"
+  },
+  {
+    "revision": "83ec443dbaee11d6f020",
+    "url": "/react-sports-game-assessment/static/js/2.fbb68554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sports-game-assessment/static/js/2.fbb68554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ffce9fe61eddb0eb78",
+    "url": "/react-sports-game-assessment/static/js/main.b9e46e34.chunk.js"
+  },
+  {
+    "revision": "869bfc85debc3fcd1f5b",
+    "url": "/react-sports-game-assessment/static/js/runtime-main.4bd516d6.js"
+  },
+  {
+    "revision": "edb1b73edaab9e9eae88c23b1a24308b",
+    "url": "/react-sports-game-assessment/static/media/Back+Board.edb1b73e.wav"
+  },
+  {
+    "revision": "3473f71ee466eb08e7a92e703e5aec43",
+    "url": "/react-sports-game-assessment/static/media/Ball+Hit+Cheer.3473f71e.wav"
+  },
+  {
+    "revision": "ed106cd0590ce875ea4faeae69a4d2c6",
+    "url": "/react-sports-game-assessment/static/media/allStars.ed106cd0.png"
+  },
+  {
+    "revision": "ad156338c6ce2d5efbf8f5ae76b1e2d9",
+    "url": "/react-sports-game-assessment/static/media/blazers.ad156338.jpg"
+  }
+]);
